98d2fd9c2b503c863dbe0ad9455b056d397e0d48
codelab21 <codelab21@outlook.com>
2023-08-10 19:42:04 +0300

    Initial Commit

---
+++ SwiftUICourse/SwiftUICourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICourseApp.swift
+//  SwiftUICourse
+//
+//  Created by Eymen on 10.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
